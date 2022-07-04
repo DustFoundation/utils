ae947a441437ca83fd4a8a88b6a7405f9c1ea07b
Hibanka <hibankaoff@gmail.com>
2022-07-04 10:54:43 +0300

    feat: streamToBuffer

---
+++ src/functions/stream-to-buffer.ts
@@ -0,0 +1,10 @@
+import { Readable } from 'node:stream';
+
+export async function streamToBuffer(stream: Readable): Promise<Buffer> {
+  return await new Promise((resolve, reject) => {
+    const chunks: Uint8Array[] = [];
+    stream.on('data', (chunk) => chunks.push(chunk));
+    stream.on('error', reject);
+    stream.on('end', () => resolve(Buffer.concat(chunks)));
+  });
+}
